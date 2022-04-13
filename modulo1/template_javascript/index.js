9c3ea6da0dd922551ae8f76e429fbe27548d16dc
gans92 <gabri.alisson.23@gmail.com>
2022-04-12 22:50:32 -0500

    template_javascript

---
+++ modulo1/template_javascript/index.js
@@ -0,0 +1,82 @@
+exercicios de interpletação
+
+1 
+
+no primeiro console irá mostrar no terminal do navegador somente o numero 5
+no segundo console irá mostar os numeros 10 e 5, porque foi atribuido um novo valor
+na variavel let b.
+
+2 
+
+no console irá mostrar os seguintes numeros respectivamente 10, 10, 10.
+
+3 
+ele pergunta ao usuario "Quantas horas você trabalha por dia?" e depois
+irá perguntar "Quanto você recebe por dia?".
+Após essas perguntas, irá aparecer no navegador a seguinte mensagem "Você recebe X por hora".
+
+let p trocar por: let horasTrabalhadas
+let t trocar por: let valorDiario
+
+exercicios de escrita
+
+1 
+a e b
+const seuNome = prompt("Qual o seu nome?")
+const suaIdade = prompt("Qual a sua idade?")
+
+c
+console.log(typeof nome)
+console.log(typeof idade)
+
+d 
+No terminal imprimiu a palavra undefined, pois ainda não atribuimos valores nas variaveis * /
+
+e
+let nome = seuNome
+let idade = suaIdade
+
+f
+atribuindo valores às minhas variaveis, o tipo retornado é strings.
+
+    g
+console.log("Olá", nome, ", você tem", idade, "anos.")
+
+2
+
+const alimentacao = prompt("Você jantou?")
+const estudos = prompt("Você estudou hoje?")
+const idade = prompt("qual sua idade?)
+
+const suaAlimentacao = alimentacao
+const seusEstudos = estudos
+const suaIdade = idade
+
+console.log("Você almoçou?", suaAlimentacao, ". Você estudou hoje?", seusEstudos, ". qual sua idade?", suaIdade)
+
+3
+
+let a = 25
+let b = 10
+
+c = a
+a = b
+b = c
+
+console.log("O valor de a é:", a)
+console.log("O valor de b é:", b)
+
+desafio
+
+let primeiroNumero = prompt("Informe o primeiro número:")
+let segundoNumero = prompt("Informe o segundo número:")
+
+let numeroPrimeiro
+let numeroSegundo
+
+numeroPrimeiro = Number(primeiroNumero)
+numeroSegundo = Number(segundoNumero)
+
+console.log("A adição do primeiro número com o segundo número é:", numeroPrimeiro + numeroSegundo)
+console.log("A multiplicação do primeiro numero com o segundo número é:", numeroPrimeiro * numeroSegundo)
+
