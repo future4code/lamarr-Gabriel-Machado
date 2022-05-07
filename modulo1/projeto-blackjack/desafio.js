ef977227a2fad6fccbf594dff5f53892fd195813
gans92 <gabri.alisson.23@gmail.com>
2022-05-06 20:40:57 -0500

    projeto-blackjack

---
+++ modulo1/projeto-blackjack/desafio.js
@@ -0,0 +1,45 @@
+//console.log("Boas vindas ao jogo de Blackjack!");
+
+let usuarioPontuacao1 = comprarCarta()
+let usuarioPontuacao2 = comprarCarta()
+let somaUsuario = usuarioPontuacao1.valor + usuarioPontuacao2.valor
+
+let pcPontuacao1 = comprarCarta()
+let pcPontuacao2 = comprarCarta()
+let somaPc = pcPontuacao1.valor + pcPontuacao2.valor
+
+// if (confirm("Quer inicar uma nova rodada? ok ou cancelar.")) {
+//    console.log(`Usuário - cartas: ${usuarioPontuacao1.texto} ${usuarioPontuacao2.texto} - pontuação ${somaUsuario}`)
+//    console.log(`Computador - cartas: ${pcPontuacao1.texto} ${pcPontuacao2.texto} - pontuação ${somaPc}`)
+//    if (somaUsuario > somaPc) {
+//       console.log(`Usuário ganhou!`)
+//    } else if (somaPc > somaUsuario) {
+//       console.log(`Computador ganhou!`)
+//    } else if (somaUsuario === somaPc) {
+//       console.log(`Empate!`)
+//    }
+// } else {
+//    console.log("O jogo acabou.")
+// }
+
+//desafii
+
+// (usuarioPontuacao1.texto && usuarioPontuacao2.texto !== "A")
+//    let usuarioNovapontuacao1 = comprarCarta()
+//    let usuarioNovapontuacao2 = comprarCarta()
+//    let usuarioPontuacao1 = usuarioNovapontuacao1
+//    let usuarioPontuacao2 = usuarioNovapontuacao2
+
+if (confirm("Bem-vinde ao jogo de Blackjack! \nQuer iniciar uma nova rodada?")) {
+   console.log()
+   if (confirm(`Suas cartas são ${usuarioPontuacao1.texto} ${usuarioPontuacao2.texto}.  A carta revelada do computador é ${pcPontuacao1.texto} \nDeseja comprar mais uma carta?`)) {
+      console.log()
+   } else if (alert(
+      `Usuario - cartas: ${usuarioPontuacao1.texto} ${usuarioPontuacao2.texto} - Pontuação: ${somaUsuario} \nComputador- cartas: ${pcPontuacao1.texto} ${pcPontuacao2.texto} - Pontuação: ${somaPc}`)) {
+      console.log(somaPc>somaUsuario === "pc ganhou")
+   } else if (somaPc>somaUsuario){
+      console.log("pc ganhou!")
+   }
+} else {
+   console.log();
+}
