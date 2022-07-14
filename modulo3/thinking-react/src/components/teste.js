8928c997b18bbfc7a95674bdcac732aa90ceb17f
gans92 <gabri.alisson.23@gmail.com>
2022-07-14 17:00:55 -0500

    thinking-react

---
+++ modulo3/thinking-react/src/components/teste.js
@@ -0,0 +1,5 @@
+export const teste = [
+    {id:"", nome:"", preço:"", descrição:"", imagem:""},
+    {id:"", nome:"", preço:"", descrição:"", imagem:""},
+    {id:"", nome:"", preço:"", descrição:"", imagem:""}
+]
