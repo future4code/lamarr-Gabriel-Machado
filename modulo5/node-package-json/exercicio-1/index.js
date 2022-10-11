93da68317f4e82de5864610943f26d7b9bd75638
gans92 <gabri.alisson.23@gmail.com>
2022-10-11 16:35:12 -0500

    letra a

--- modulo5/node-package-json/exercicio-1/index.js
+++ modulo5/node-package-json/exercicio-1/index.js
@@ -1,3 +1,6 @@
+//a)
+//Basta utilizar process. argv[2]. Essa propriedade irá armazenar uma cópia, somente leitura, do valor original de argv[0] passado quando o arquivo. js for iniciado.
+//b)
 const nome = process.argv[2];
 const idade = Number(process.argv[3]);
 
