ee1d97ddc7bc419346e026a9e7acd1edddbd4fda
gans92 <gabri.alisson.23@gmail.com>
2022-11-19 09:22:31 -0500

    updates

--- modulo5/knex-js/src/dataBase/connection.ts
+++ modulo5/knex-js/src/dataBase/connection.ts
@@ -9,7 +9,7 @@ const connection = knex({
         host:process.env.DB_HOST,
         port:3306,
         user:process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
+        password:process.env.DB_PASS,
         database:process.env.DB_SCHEMA,
         multipleStatements:true
     }
