9dfc5b693de6582f7d476b60ef6ec82d41d6352c
gans92 <gabri.alisson.23@gmail.com>
2023-03-21 21:50:24 -0500

    resolvendo-problemas

---
+++ resolvendo-problemas/src/index.ts
@@ -0,0 +1,6 @@
+import { isOneEdit } from './exercicio1'
+
+console.log("// Exercicio 1 //")
+console.log(isOneEdit("banana","bananao"))
+console.log(isOneEdit("banana","batata"))
+console.log(isOneEdit("banana","bananaao"))
